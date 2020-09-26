25dfb322977a5ff32ce6f203c06db27854df13c9
ReiSogawa <natto.and.eggs@gmail.com>
2020-09-26 14:12:08 +0900

    FIX: store

--- src/store/modules/questions.js
+++ src/store/modules/questions.js
@@ -18,20 +18,20 @@ const actions = {
     snapshot.forEach(doc => {
       questions.push({ id: doc.id, title: doc.data().title })
     })
-    commit('SET_QUESTIONS', questions)
+    commit('SET_QUESTIONS', { questions })
   },
 
   async addQuestion({ commit }, newQuestion) {
     await db.collection('questions').add(newQuestion)
-    commit('ADD_QUESTION', newQuestion)
+    commit('ADD_QUESTION', { newQuestion })
   }
 }
 
 const mutations = {
-  SET_QUESTIONS(state, questions) {
+  SET_QUESTIONS(state, { questions }) {
     state.questions = questions
   },
-  ADD_QUESTION(state, newQuestion) {
+  ADD_QUESTION(state, { newQuestion }) {
     state.questions.push(newQuestion)
   }
 }
